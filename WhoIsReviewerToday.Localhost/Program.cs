77182a56141d7d157e3264f3fe19ad4ffa18e601
ppetrov <pppetrov92@google.com>
2018-12-26 15:50:49 +0200

    intorduce WhoIsReviewerToday.Localhost for local tests.

---
+++ WhoIsReviewerToday.Localhost/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Telegram.Bot.Args;
+using WhoIsReviewerToday.Bot;
+using WhoIsReviewerToday.Domain;
+using WhoIsReviewerToday.Infrastructure.Services;
+
+namespace WhoIsReviewerToday.Localhost
+{
+    public class Program
+    {
+        private static IServiceProvider _webHostServices;
+
+        public static void Main(string[] args)
+        {
+            var webHost = CreateWebHostBuilder(args).Build();
+            _webHostServices = webHost.Services;
+
+            using (var scope = _webHostServices.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+
+                var dbInitializer = serviceProvider.GetRequiredService<IDbInitializer>();
+                dbInitializer.SeedIfNeeded();
+            }
+
+            var whoIsReviewerTodayBot = _webHostServices.GetRequiredService<IWhoIsReviewerTodayBot>();
+            whoIsReviewerTodayBot.StartReceiving();
+            whoIsReviewerTodayBot.OnUpdate += OnUpdate;
+
+            webHost.Run();
+
+            whoIsReviewerTodayBot.OnUpdate -= OnUpdate;
+            whoIsReviewerTodayBot.StopReceiving();
+        }
+
+        private static void OnUpdate(object sender, UpdateEventArgs e)
+        {
+            using (var scope = _webHostServices.CreateScope())
+            {
+                var updateService = scope.ServiceProvider.GetRequiredService<IUpdateService>();
+                updateService.Update(e.Update);
+            }
+        }
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ WhoIsReviewerToday.Localhost/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WhoIsReviewerToday.Bot;
+using WhoIsReviewerToday.Infrastructure;
+using WhoIsReviewerToday.Infrastructure.EntityFramework;
+
+namespace WhoIsReviewerToday.Localhost
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private void AddApplicationClassesRegistrations(IServiceCollection services)
+        {
+            var botToken = _configuration["BotForTest"];
+
+            services.SetupWhoIsReviewerTodayBotAndService(botToken)
+                .SetupDbContext("Server=(localdb)\\MSSQLLocalDB;Database=WhoIsReviewerToday;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .SetupDbInitializer()
+                .SetupProviders()
+                .SetupServices()
+                .SetupCommands()
+                .SetupFactories()
+                .SetupRepositories();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            AddApplicationClassesRegistrations(services);
+        }
+
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env)
+        {
+            app.Run(
+                async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+        }
+    }
+}
